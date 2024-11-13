2a7c3d931225ba113f1a02e9d93cbacd867fcfef
CppCXY <812125110@qq.com>
2024-11-13 14:50:44 +0800

    remove highlightfilter

--- src/main/java/com/tang/intellij/lua/highlighting/LuaHighlightErrorFilter.kt
+++ src/main/java/com/tang/intellij/lua/highlighting/LuaHighlightErrorFilter.kt
@@ -16,19 +16,20 @@
 
 package com.tang.intellij.lua.highlighting
 
-import com.intellij.codeInsight.highlighting.HighlightErrorFilter
-import com.intellij.psi.PsiErrorElement
-import com.tang.intellij.lua.comment.psi.LuaDocPsiElement
-import com.tang.intellij.lua.psi.LuaPsiElement
-
-//import com.tang.intellij.lua.project.LuaSettings
+//import com.intellij.codeInsight.highlighting.HighlightErrorFilter
+//import com.intellij.psi.PsiErrorElement
+//import com.tang.intellij.lua.comment.psi.LuaDocPsiElement
+//import com.tang.intellij.lua.psi.LuaPsiElement
+//
+////import com.tang.intellij.lua.project.LuaSettings
 
 /**
  * disable error highlight for doc elements
  * Created by tangzx on 2017/2/19.
  */
-class LuaHighlightErrorFilter : HighlightErrorFilter() {
-    override fun shouldHighlightErrorElement(psiErrorElement: PsiErrorElement): Boolean {
-        return psiErrorElement !is LuaPsiElement
-    }
-}
+//class LuaHighlightErrorFilter : HighlightErrorFilter() {
+//    override fun shouldHighlightErrorElement(psiErrorElement: PsiErrorElement): Boolean {
+////        return psiErrorElement !is LuaPsiElement
+//        return true
+//    }
+//}

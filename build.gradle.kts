685c3b19db29a4cf6ff5ea16c6ce3d056b7fbe5b
CppCXY <812125110@qq.com>
2024-11-14 20:33:55 +0800

    fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ val buildDataList = listOf(
 )
 
 group = "com.cppcxy"
-val sumnekoVersion = "3.13.1"
+val sumnekoVersion = "3.13.0"
 
 val sumnekoProjectUrl = "https://github.com/LuaLS/lua-language-server"
 
